cd4bf4b7e0b75f2214f455b53c27b554e0b569db
Jajacatte <jajacatte@gmail.com>
2024-04-21 01:44:41 +0100

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000", "https://appointment-server-15eu.onrender.com"],
   // Add other CORS options as needed
 };
 
